b21e1b657d44b26c0edc4cf791884cc7a8d1ce74
Abdikani98 <abdikanishaarib>
2024-12-23 15:12:10 -0800

    new

---
+++ main.js
@@ -0,0 +1,104 @@
+// document.getElementById("tittle").style.color = "red"
+
+// let elements = document.getElementById("tittle")
+
+// elements.style.color = "white"
+// elements.style.backgroundColor = "blue"
+
+// elements.style.paddingLeft = "20px"
+// elements.style.marginRight = "40px"
+// elements.style.fontSize = "2em"
+// elements.style.borderRadius = "20px"
+// elements.style.boxShadow = "5px 5px 10px black"
+// elements.style.display = "flex "
+// elements.style.gap = "20% "
+
+
+// let test = document.getElementById("tittle2")
+
+// test.style.display = "flex"
+// test.style.gap = "10%"
+// test.style.listStyleType = "none"
+// test.style.paddingTop = "18px"
+// test.style.fontSize = "1.2em"
+
+
+
+// let element = document.getElementById("tittle3")
+
+// element.style.paddingTop = "10px"
+// element.style.fontSize = "40px"
+
+
+
+
+// let shiwan = document.querySelector("#shiwa")
+
+// shiwan.style.color = "white"
+// shiwan.style.backgroundColor = "black"
+
+// shiwan.style.paddingLeft = "20px"
+// shiwan.style.marginRight = "40px"
+// shiwan.style.marginTop = "10%"
+// shiwan.style.fontSize = "2em"
+// shiwan.style.borderRadius = "20px"
+// shiwan.style.boxShadow = "5px 5px 10px black"
+// shiwan.style.display = "flex "
+// shiwan.style.gap = "20% "
+
+
+
+// let shiwan1 = document.querySelector("#shiwa2")
+
+// shiwan1.style.display = "flex"
+// shiwan1.style.gap = "10%"
+// shiwan1.style.listStyleType = "none"
+// shiwan1.style.paddingTop = "18px"
+// shiwan1.style.fontSize = "1.2em"
+
+
+// let shiwan2 = document.querySelector("shiwa1")
+
+// shiwan2.style.paddingTop = "10px"
+// shiwan2.style.fontSize = "10px"
+
+
+
+// let btn = document.querySelector(".btn")
+
+// btn.addEventListener("click", () => {
+//     document.body.style.backgroundColor = "red"
+  
+// })
+// let btn2 = document.querySelector(".btn2")
+
+// btn2.addEventListener("click", () => {
+//     document.body.style.backgroundColor = "white"
+  
+// })
+
+
+let hiden = document.querySelector ("#hidden")
+let delte =document.querySelector ("#abdi")
+
+hiden.addEventListener ("click", () => {
+    delte.style.display = "none"
+    hidden.style.backgroundColor = "red"
+    
+})
+
+
+
+
+let dispaly = document.querySelector ("#display")
+let muji =document.querySelector ("#abdi")
+
+dispaly.addEventListener ("click", () => {
+    muji.style.display = "flex"
+    display.style.backgroundColor ="yellow"
+
+})
+
+
+
+
